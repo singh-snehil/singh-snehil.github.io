1f321c86b2d1c2fa655ce38071c4de8e39d1b2a3
singh-snehil <singh-snehil@users.noreply.github.com>
2022-09-08 15:50:03 +0000

    deploy: a7a53da90abbac7c8cf89090958e5d2fe44b31ba

---
+++ js/volunteers.js
@@ -0,0 +1,8 @@
+let badges = document.querySelectorAll('.volunteer-badge');
+
+badges.forEach(badge => {
+    if(badge.dataset.image.length === 0) {
+        let initials = badge.dataset.name.split(' ').map(str => {if(str !== 'Dr.') {return str[0]}}).join('');
+        badge.textContent = initials;
+    }
+});
